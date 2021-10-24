718121334c7f49a43559046c4ce14a6f4e29a561
LCB <temagastudio@gmail.com>
2021-10-24 17:27:08 +0500

    DONE LW1

--- lw1/lw1.asm
+++ lw1/lw1.asm
@@ -1,16 +1,24 @@
 .model small
-.stack 256
-.data
-num db +56
-numtwo dw -350
 
+.stack
+
+.data
+num db 56
+numtwo dw -350
 fort dd -456986
+
 .code
 start:
-	add bx, 345h
-	mov bx, cx
-	mov bx, bp
-	mov bx, ax
-	xchg numtwo, di
-	
+	mov bx, 345h
+	mov cx, bx
+	mov bp, bx
+	xchg al, num
+	xchg di, numtwo
+	push fort
+	push fort+2
+	pop fort
+	pop fort+2
+	xchg al, ch
+	mov ax, 4c00h
+	int 21h
 end start
